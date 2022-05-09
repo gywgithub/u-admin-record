8adffd5d85a8b6e53d6adb13f0c7b5a9e4173d4b
chengIronMan <c18145428744@163.com>
2022-05-09 20:48:30 +0800

    修改cxy IP地址

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,8 @@ module.exports = {
 		},
 		proxy: {
 			"/api": {
-				target: "http://10.255.50.70:8080", //gs
+				// target: "http://10.255.50.70:8080", //gs
+				target: "http://10.0.0.172:8080", //cxy
 				// target: "http://192.168.124.14:8080", //jia
 				ws: true,
 				changeOrigin: true,
